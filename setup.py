bb85ebe57afe3f30e381db673b6a3004f4acdc1d
BAEM1N <baemin.dev@gmail.com>
2021-10-05 14:39:09 +0900

    init commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+setup(
+    name='JAEN',
+    version='0.0.1',
+    description='Project Submission',
+    author='BAEM1N',
+    author_email='baemin.dev@gmail.com',
+    url='https://github.com/BAEM1N/JAEN',
+    download_url='https://github.com/BAEM1N/JAEN/archive/master.zip',
+    install_requires=[],
+    packages=find_packages(exclude=[]),
+    keywords=['AI', 'ML', 'DL'],
+    python_requires='>=3',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
